f1630dc4468f6374b8dbfbb22b70cb06c6b3cb4b
Andrew Bunning <andrew@ABunning.com>
2024-03-03 17:48:36 +0000

    Update app.js with datetime

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
+/* function App() {
   const locale = 'en';
   const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
 
   return <div>Hello {value}. The current time is {time}</div>;
+}
+
+export default App; */
+
+
+class App extends React.Component {
+  state={
+    curDT : new Date().toLocaleString(),
+  }
+  render(){
+    return (
+      <div className="App">
+        <p>Hello World!</p>
+        <p>Current Date And Time : {this.state.curDT}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
