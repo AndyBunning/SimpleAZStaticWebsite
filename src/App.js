7bbb0fcec57ce9f6656ebd023dbda841dabbff13
Andrew Bunning <andrew@ABunning.com>
2024-03-03 17:39:33 +0000

    Added Datetime to Angular page.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const locale = 'en';
+  const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
+
+  return <div>Hello {value}. The current time is {time}</div>;
 }
 
 export default App;
